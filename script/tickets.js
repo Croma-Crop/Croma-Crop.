78a66233a9c287c73604c04cd65b3788ce903dc1
Santiagoy <767pela@gmail.com>
2026-06-08 10:55:41 -0300

    Puliendo otros modulos y creacionn del modulo inventario

--- script/tickets.js
+++ script/tickets.js
@@ -24,25 +24,29 @@ const btnEnviar = document.querySelector("#enviarinc")
 
 btnEnviar.addEventListener("click", function(e){
      e.preventDefault();
-
+    const nombreprof = document.getElementById("nombreProf").value;
     const fecha = document.getElementById("fecha").value;
     const salon = document.getElementById("salon").value;
     const turno = document.querySelector('input[name="turno"]:checked')?.value;
     const tipo = document.getElementById("tipo").value;
     const tipoincidencia = document.getElementById("descripcioninc").value;
 
-    alert("Fecha: " + fecha + "\nSalon: " + salon + "\nTurno: " + turno + "\nTipo: " + tipo + "\nIncidencia: " + tipoincidencia);
-
+    alert("Nombre Profesor: " + nombreprof + "\nFecha: " + fecha + "\nSalon: " + salon + "\nTurno: " + turno + "\nTipo: " + tipo + "\nIncidencia: " + tipoincidencia);
+    const form = document.querySelector("#incforms");
+    form.reset();
 })
 const enviarSol = document.querySelector("#enviarSol")
 
 enviarSol.addEventListener("click", function(e){
      e.preventDefault();
 
+    const nombreprof = document.getElementById("nombreProfSol").value;
     const tipoSol = document.getElementById("tipoSol").value;
     const descripcionSol = document.getElementById("descripcionSol").value;
 
-    alert("Tipo de solicitud: " + tipoSol + "\nDescripcion: " + descripcionSol);
+    alert("Nombre Profesor: " + nombreprof + "\nTipo de solicitud: " + tipoSol + "\nDescripcion: " + descripcionSol);
+    const form = document.querySelector("#solforms");
+    form.reset();
 
 })
 
