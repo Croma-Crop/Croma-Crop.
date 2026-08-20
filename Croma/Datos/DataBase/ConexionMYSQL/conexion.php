e01be239a88de383e34c445ffc8f415f5781c145
root <andressosa1238@gmail.com>
2026-08-19 22:04:19 -0300

    Cambios en la conexion

--- Croma/Datos/DataBase/ConexionMYSQL/conexion.php
+++ Croma/Datos/DataBase/ConexionMYSQL/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$conexion = mysqli_connect("localhost", "root", "", "srgsi");
+$conexion = mysqli_connect("192.168.1.7", "root", "croma", "srgsi");
 
 if (!$conexion) {
     die("Error de conexión: " . mysqli_connect_error());
